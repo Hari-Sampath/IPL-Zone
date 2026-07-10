d3d6566391d4aeb663e4e4e576d0f799fc4adfcb
Hari Sampath <harisampathbellam@gmail.com>
2026-07-10 13:31:46 +0530

    Initial commit

---
+++ src/test/java/com/example/IPL_Zone/IplZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IPL_Zone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
